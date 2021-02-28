31ed06ff9e3ece5c5fe7a6d883267a792ef5c0ae
MNigar <nigar-4t@live.com>
2021-02-28 12:53:29 -0800

    added new temp

--- LawyerTemplate/Controllers/HomeController.cs
+++ LawyerTemplate/Controllers/HomeController.cs
@@ -15,14 +15,20 @@ namespace LawyerTemplate.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+          
+
+            return View();
+        }
+        public ActionResult Team()
+        {
+            
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            
 
             return View();
         }
